c849417deb964f60123b2f259fddc1f9fec2d079
unknown <adri.cadoret@gmail.com>
2018-02-20 11:04:29 +0100

    Neo4J getConnectionForKevinBacon

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/api/APIEndPoint.java
@@ -1,10 +1,16 @@
 package com.serli.oracle.of.bacon.api;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
 import com.serli.oracle.of.bacon.repository.MongoDbRepository;
 import com.serli.oracle.of.bacon.repository.Neo4JRepository;
 import com.serli.oracle.of.bacon.repository.RedisRepository;
 import net.codestory.http.annotations.Get;
+import org.json.JSONObject;
+import org.neo4j.driver.v1.Value;
+import org.neo4j.driver.v1.types.Node;
+import org.neo4j.driver.v1.types.Relationship;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -26,45 +32,42 @@ public class APIEndPoint {
     @Get("bacon-to?actor=:actorName")
     public String getConnectionsToKevinBacon(String actorName) {
 
-        return "[\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 85449,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Bacon, Kevin (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 2278636,\n" +
-                "\"type\": \"Movie\",\n" +
-                "\"value\": \"Mystic River (2003)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 1394181,\n" +
-                "\"type\": \"Actor\",\n" +
-                "\"value\": \"Robbins, Tim (I)\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 579848,\n" +
-                "\"source\": 85449,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "},\n" +
-                "{\n" +
-                "\"data\": {\n" +
-                "\"id\": 9985692,\n" +
-                "\"source\": 1394181,\n" +
-                "\"target\": 2278636,\n" +
-                "\"value\": \"PLAYED_IN\"\n" +
-                "}\n" +
-                "}\n" +
-                "]";
+        List<Object> elements = (List<Object>) neo4JRepository.getConnectionsToKevinBacon(actorName);
+
+
+        JsonArray jsonArray = new JsonArray();
+        for(Object element : elements){
+            if(element instanceof Node) {
+                Node node = (Node) element;
+                JsonObject dataObject = new JsonObject();
+                JsonObject nodeObject = new JsonObject();
+
+                nodeObject.addProperty("id", node.id());
+                for (String label : node.labels()) {
+                    nodeObject.addProperty("type", label);
+                }
+
+                for (Value value : node.values()) {
+                    nodeObject.addProperty("value", value.toString());
+                }
+                dataObject.add("data", nodeObject);
+                jsonArray.add(dataObject);
+            }
+            if(element instanceof Relationship){
+                Relationship relationship = (Relationship) element;
+                JsonObject dataObject = new JsonObject();
+                JsonObject relationshipObject = new JsonObject();
+                relationshipObject.addProperty("id", relationship.id());
+                relationshipObject.addProperty("source", relationship.startNodeId());
+                relationshipObject.addProperty("target", relationship.endNodeId());
+                relationshipObject.addProperty("value", relationship.type());
+
+                dataObject.add("data", relationshipObject);
+                jsonArray.add(dataObject);
+            }
+        }
+
+        return jsonArray.toString();
     }
 
     @Get("suggest?q=:searchQuery")
